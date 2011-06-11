a4855470738354ac13ef0a1e0ab0f519ee730583
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2011-06-10 18:40:13 -0700

    the initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodwrks::Application.config.secret_token = 'c14ac8433a6077590e3500156ebefddeb8e57c3cd7190042c4b9924d1bc8312ef7cf12199ddceb40b1291d8662e42c108cc40edce2c2eb571a602990d1d57867'
